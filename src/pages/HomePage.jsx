69d5841ee7ef29bb11f7a7325c7ebd0424acfd9e
Neslihan Yağmurca <neslihanyagmurca@Neslihan-MacBook-Air.local>
2024-02-17 18:15:20 +0300

    frontend

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Header from "../components/Layout/Header/Header";
+import Sliders from "../components/Slider/Sliders";
+import Categories from "../components/Categories/Categories";
+import Products from "../components/Products/Products";
+import Campaigns from "../components/Campaigns/Campaigns";
+import Blogs from "../components/Blogs/Blogs";
+import Brands from "../components/Brands/Brands";
+import CampaignSingle from "../components/CampaignSingle/CampaignSingle";
+import Policy from "../components/Layout/Policy/Policy";
+import Footer from "../components/Layout/Footer/Footer";
+
+const HomePage = () => {
+  return (
+    <React.Fragment>
+      <Sliders />
+      <Categories />
+      <Products />
+      <Campaigns />
+      <Products />
+      <Blogs />
+      <Brands />
+      <CampaignSingle />
+    </React.Fragment>
+  );
+};
+
+export default HomePage;
